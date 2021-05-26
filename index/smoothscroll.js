60d0804aa6f26fc7b7522d9a1d5002a24a34f32c
boleth <74958282+boleth@users.noreply.github.com>
2021-05-26 08:56:42 +0200

    index

---
+++ index/smoothscroll.js
@@ -0,0 +1,31 @@
+//https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+
+document.querySelectorAll('a[href^="#produkter"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+document.querySelectorAll('a[href^="#omos"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+document.querySelectorAll('a[href^="#kontakt"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
