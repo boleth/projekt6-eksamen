a9a383621f139309661c0983862e45ab0a9ff521
sarahsorenseen <sarah.sorenseen@gmail.com>
2021-05-26 08:58:14 +0200

    Menu

---
+++ hamburgermenu.js
@@ -0,0 +1,13 @@
+function myFunction(e){
+  e.classList.toggle("show");
+
+  var elem = document.getElementById("navID"),
+  Style = window.getComputedStyle(elem),
+  right = Style.getPropertyValue("right");
+
+  if(right == "0px"){
+    elem.style.right = "-260px";
+  }else{
+    elem.style.right = "0px";
+  }
+}
